ea2ca7a90ce105920c6ff142d496f8bcda5495fe
Michał Białecki <michal.bialecki@guestline.com>
2020-05-01 22:21:13 +0200

    Generate profiles and bulk safe

--- PrimeHotel.Web/Controllers/ProfileController.cs
+++ PrimeHotel.Web/Controllers/ProfileController.cs
@@ -1,7 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using System.Threading.Tasks;
+using Bogus;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using PrimeHotel.Web.Models;
 
 namespace PrimeHotel.Web.Controllers
@@ -11,9 +17,11 @@ namespace PrimeHotel.Web.Controllers
     public class ProfileController : ControllerBase
     {
         private readonly PrimeDbContext primeDbContext;
+        private readonly string connectionString;
 
-        public ProfileController(PrimeDbContext _primeDbContext)
+        public ProfileController(PrimeDbContext _primeDbContext, IConfiguration _configuration)
         {
+            connectionString = _configuration.GetConnectionString("HotelDB");
             primeDbContext = _primeDbContext;
         }
 
@@ -78,6 +86,75 @@ namespace PrimeHotel.Web.Controllers
             await primeDbContext.SaveChangesAsync();
 
             return Ok(removedProfile.Entity);
+        }
+
+        [HttpPost("GenerateAndInsert")]
+        public async Task<IActionResult> GenerateAndInsert([FromBody] int count = 1000)
+        {
+            Stopwatch s = new Stopwatch();
+            s.Start();
+
+            var profiles = GenerateProfiles(count);
+            var gererationTime = s.Elapsed.ToString();
+            s.Restart();
+
+            primeDbContext.Profiles.AddRange(profiles);
+            var insertedCount = await primeDbContext.SaveChangesAsync();
+
+            return Ok(new {
+                    inserted = insertedCount,
+                    generationTime = gererationTime,
+                    insertTime = s.Elapsed.ToString()
+                });
+        }
+
+        [HttpPost("GenerateAndInsertWithSqlCopy")]
+        public async Task<IActionResult> GenerateAndInsertWithSqlCopy([FromBody] int count = 1000)
+        {
+            Stopwatch s = new Stopwatch();
+            s.Start();
+
+            var profiles = GenerateProfiles(count);
+            var gererationTime = s.Elapsed.ToString();
+            s.Restart();
+
+            var dt = new DataTable();
+            dt.Columns.Add("Id");
+            dt.Columns.Add("Ref");
+            dt.Columns.Add("Forename");
+            dt.Columns.Add("Surname");
+            dt.Columns.Add("Email");
+            dt.Columns.Add("TelNo");
+            dt.Columns.Add("DateOfBirth");
+
+            foreach (var profile in profiles)
+            {
+                dt.Rows.Add(string.Empty, profile.Ref, profile.Forename, profile.Surname, profile.Email, profile.TelNo, profile.DateOfBirth);
+            }
+
+            using var sqlBulk = new SqlBulkCopy(connectionString);
+            sqlBulk.DestinationTableName = "Profiles";
+            await sqlBulk.WriteToServerAsync(dt);
+
+            return Ok(new
+            {
+                inserted = dt.Rows.Count,
+                generationTime = gererationTime,
+                insertTime = s.Elapsed.ToString()
+            });
+        }
+
+        private IEnumerable<Profile> GenerateProfiles(int count)
+        {
+            var profileGenerator = new Faker<Profile>()
+                .RuleFor(p => p.Ref, v => v.Person.UserName)
+                .RuleFor(p => p.Forename, v => v.Person.FirstName)
+                .RuleFor(p => p.Surname, v => v.Person.LastName)
+                .RuleFor(p => p.Email, v => v.Person.Email)
+                .RuleFor(p => p.TelNo, v => v.Person.Phone)
+                .RuleFor(p => p.DateOfBirth, v => v.Person.DateOfBirth);
+
+            return profileGenerator.Generate(count);
         }
     }
 }
